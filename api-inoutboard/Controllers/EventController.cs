d0b2559f26875189a30df8b4e06cc8407e71d19b
Rich Eodice <rich@eodice.com>
2023-10-19 12:20:45 -0400

    Added Events controller basics

---
+++ api-inoutboard/Controllers/EventController.cs
@@ -0,0 +1,86 @@
+﻿using api_inoutboard.models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace api_inoutboard.Controllers
+{
+    [ApiController]
+    public class EventController : Controller
+    {
+        [Route("api/events/seed")]
+        [HttpGet]
+        public ActionResult Seed()
+        {
+            try
+            {
+                var dbClient = new MongoClient(Configuration.dbconnection);
+                var database = dbClient.GetDatabase(Configuration.dbname);
+                var collection = database.GetCollection<Event>(Configuration.eventscollection);
+
+                List<Event> events = new List<Event>
+                {
+                    new Event(80762, new DateTime(2023,10,16,8,0,0), new DateTime(2023,10,16,16,0,0), 108982, ""),
+                    new Event(80762, new DateTime(2023,10,17,8,0,0), new DateTime(2023,10,17,16,0,0), 108982, ""),
+                    new Event(80762, new DateTime(2023,10,18,7,30,0), new DateTime(2023,10,18,15,30,0), 792128, ""),
+                    new Event(80762, new DateTime(2023,10,19,8,0,0), new DateTime(2023,10,19,16,0,0), 108982, ""),
+                    new Event(80762, new DateTime(2023,10,20,8,0,0), new DateTime(2023,10,20,16,0,0), 108982, "")
+                };
+
+                collection.InsertMany(events);
+
+                var results = collection.AsQueryable()
+                .ToList();
+
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Route("api/events/today")]
+        [HttpGet]
+        public ActionResult GetAllToday()
+        {
+            try
+            {
+                var dbClient = new MongoClient(Configuration.dbconnection);
+                var database = dbClient.GetDatabase(Configuration.dbname);
+                var collection = database.GetCollection<Event>(Configuration.eventscollection);
+
+                var results = collection.AsQueryable()
+                    .Where(x => x.Start.DayOfYear == DateTime.Now.DayOfYear)
+                    .ToList();
+
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [Route("api/events/user/{userid}")]
+        [HttpGet]
+        public ActionResult GetAllUser(int userid)
+        {
+            try
+            {
+                var dbClient = new MongoClient(Configuration.dbconnection);
+                var database = dbClient.GetDatabase(Configuration.dbname);
+                var collection = database.GetCollection<Event>(Configuration.eventscollection);
+
+                var results = collection.AsQueryable()
+                    .Where(x => x.UserId == userid)
+                    .ToList();
+
+                return Ok(results);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
--- api-inoutboard/models/Event.cs
+++ api-inoutboard/models/Event.cs
@@ -1,14 +1,31 @@
-﻿namespace api_inoutboard.models
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace api_inoutboard.models
 {
     public class Event
     {
         public int Id { get; set; }
-        public bool UserId { get; set; }
+        public int UserId { get; set; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
         public DateTime Created { get; set; }
         public DateTime Updated { get; set; }
-        public int StatusID { get; set; }
+        public int StatusId { get; set; }
         public string Notes { get; set; }
+
+        public Event(int UserId, DateTime Start, DateTime End, int StatusId, string Notes)
+        {
+            Random rnd = new Random();
+
+            Id = rnd.Next(999999);
+            this.UserId = UserId;
+            this.Start = Start;
+            this.End = End;
+            this.StatusId = StatusId;
+            this.Notes = Notes;
+            
+            Created = DateTime.Now;
+            Updated = DateTime.Now;
+        }
     }
 }
